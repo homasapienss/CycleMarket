6851990795620c2163743e2d84dbd083019e6211
homasapienss <homa19222@gmail.com>
2026-04-07 16:28:57 +0300

    initial commit

---
+++ src/main/java/com/example/cyclemarket/CycleMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyclemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CycleMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cyclemarket/CycleMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyclemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycleMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
