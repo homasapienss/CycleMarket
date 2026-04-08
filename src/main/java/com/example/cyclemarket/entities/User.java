ac6464b37bd493edaa3118c782ea43b6a41bfd71
homasapienss <homa19222@gmail.com>
2026-04-08 17:02:38 +0300

    Добавление первых миграций и первой сущности пользователя

---
+++ src/main/java/com/example/cyclemarket/entities/User.java
@@ -0,0 +1,26 @@
+package com.example.cyclemarket.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table(name = "users")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String passwordHash;
+    private LocalDateTime createdAt;
+}
