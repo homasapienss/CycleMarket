848414c9d7ef3b02b037509324607c07e273af2d
homasapienss <homa19222@gmail.com>
2026-04-08 18:16:30 +0300

    Добавление spring-security, обьявление бинов UserDetailsService и PasswordEncoder и SecurityFilterChain

---
+++ src/main/java/com/example/cyclemarket/configs/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.cyclemarket.configs;
+
+import com.example.cyclemarket.repos.UserRepo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepo userRepo) {
+        return username -> userRepo.findByEmail(username)
+                .map(user -> new User(
+                        user.getEmail(),
+                        user.getPasswordHash(),
+                        user.getRoles().stream()
+                                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                                .toList()
+                ))
+                .orElseThrow(()->new UsernameNotFoundException(username + " not found"));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
