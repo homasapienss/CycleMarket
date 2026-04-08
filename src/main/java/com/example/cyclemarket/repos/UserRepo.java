0cfeaec34d95cbb8ad7a502f7cf001f3ba64ef4e
homasapienss <homa19222@gmail.com>
2026-04-08 17:28:39 +0300

    Добавлен репозиторий для пользователя

---
+++ src/main/java/com/example/cyclemarket/repos/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.cyclemarket.repos;
+
+import com.example.cyclemarket.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
