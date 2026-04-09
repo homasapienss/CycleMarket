9240b8d6580f0b144ed7e20242739318e9d96281
homasapienss <homa19222@gmail.com>
2026-04-09 13:05:10 +0300

    Обновление возвращаемого значения бина PasswordEncoder, пока разрешаем всем доступ к приложению

--- src/main/java/com/example/cyclemarket/configs/SecurityConfig.java
+++ src/main/java/com/example/cyclemarket/configs/SecurityConfig.java
@@ -10,13 +10,14 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -37,6 +38,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authReq -> authReq
+                        .anyRequest().permitAll())
                 .build();
     }
 }
