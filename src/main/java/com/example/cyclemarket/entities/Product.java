7d6da67c52570e8be38c040336ad947414467d39
homasapienss <homa19222@gmail.com>
2026-04-09 13:55:59 +0300

    Добавление сущности продукта

---
+++ src/main/java/com/example/cyclemarket/entities/Product.java
@@ -0,0 +1,22 @@
+package com.example.cyclemarket.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private String productDescription;
+    private String productPrice;
+}
