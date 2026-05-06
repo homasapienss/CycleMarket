c7ab21a174e463942c0ec555d05f54c4d4f4576c
homasapienss <homa19222@gmail.com>
2026-05-06 17:55:08 +0300

    Добавление репозиториев для сущностей Заказа и Позиций заказа

---
+++ src/main/java/com/example/cyclemarket/repos/OrderItemRepo.java
@@ -0,0 +1,6 @@
+package com.example.cyclemarket.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepo extends JpaRepository<Long, Integer> {
+}
---
+++ src/main/java/com/example/cyclemarket/repos/OrderRepo.java
@@ -0,0 +1,6 @@
+package com.example.cyclemarket.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<Long, Integer> {
+}
